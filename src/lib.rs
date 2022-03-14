9048b0f2823989e7b033ccf5a175f2a351a471a7
Lucas Lima <lukeflima@gmail.com>
2022-03-14 05:52:44 -0300

    Add QoiOp Enum

--- src/lib.rs
+++ src/lib.rs
@@ -60,14 +60,35 @@ const QOI_PIXELS_MAX: u32 = 400000000;
 // const QOI_HEADER_SIZE: u32 = 14;
 const QOI_END_SEGMENT_SIZE: u32 = 8;
 
-const QOI_OP_INDEX: u8 = 0x00; /* 00xxxxxx */
-const QOI_OP_DIFF: u8 = 0x40; /* 01xxxxxx */
-const QOI_OP_LUMA: u8 = 0x80; /* 10xxxxxx */
-const QOI_OP_RUN: u8 = 0xc0; /* 11xxxxxx */
-const QOI_OP_RGB: u8 = 0xfe; /* 11111110 */
-const QOI_OP_RGBA: u8 = 0xff; /* 11111111 */
+enum QoiOp {
+    Index,
+    Diff,
+    Luma,
+    Run,
+    Rgb,
+    Rgba,
+    Unknown,
+}
 
-const QOI_MASK_2: u8 = 0xc0; /* 11111111 */
+impl From<u8> for QoiOp {
+    fn from(b: u8) -> Self {
+        if b == 0xfe {
+            QoiOp::Rgb
+        } else if b == 0xff {
+            QoiOp::Rgba
+        } else if (b & 0xc0) == 0x00 {
+            QoiOp::Index
+        } else if (b & 0xc0) == 0x40 {
+            QoiOp::Diff
+        } else if (b & 0xc0) == 0x80 {
+            QoiOp::Luma
+        } else if (b & 0xc0) == 0xc0 {
+            QoiOp::Run
+        } else {
+            QoiOp::Unknown
+        }
+    }
+}
 
 fn read_32(bytes: &[u8], offset: &mut usize) -> u32 {
     let a = bytes[*offset] as u32;
@@ -115,26 +136,6 @@ impl QoiImage {
 }
 
 #[wasm_bindgen]
-pub fn check_if_valid_qoif(bytes: &[u8]) -> bool {
-    let mut index = 0_usize;
-    let header = QoiHeader {
-        magic: read_32(bytes, &mut index),
-        width: read_32(bytes, &mut index),
-        height: read_32(bytes, &mut index),
-        channels: read_8(bytes, &mut index),
-        colorspace: read_8(bytes, &mut index),
-    };
-
-    !(header.width == 0
-        || header.height == 0
-        || header.channels < 3
-        || header.channels > 4
-        || header.colorspace > 1
-        || header.magic != QOI_MAGIC
-        || header.height >= QOI_PIXELS_MAX / header.width)
-}
-
-#[wasm_bindgen]
 pub fn decode_qoi(bytes: &[u8], size: usize) -> QoiImage {
     utils::set_panic_hook();
 
@@ -155,7 +156,7 @@ pub fn decode_qoi(bytes: &[u8], size: usize) -> QoiImage {
         || header.magic != QOI_MAGIC
         || header.height >= QOI_PIXELS_MAX / header.width
     {
-        return Default::default();
+        panic!("Not a valid QOI image")
     }
 
     let px_len: usize = (header.width * header.height * header.channels as u32) as usize;
@@ -174,29 +175,37 @@ pub fn decode_qoi(bytes: &[u8], size: usize) -> QoiImage {
         } else if index < chunks_len {
             let b1 = read_8(bytes, &mut index);
 
-            if b1 == QOI_OP_RGB {
-                prev_color.r = read_8(bytes, &mut index);
-                prev_color.g = read_8(bytes, &mut index);
-                prev_color.b = read_8(bytes, &mut index);
-            } else if b1 == QOI_OP_RGBA {
-                prev_color.r = read_8(bytes, &mut index);
-                prev_color.g = read_8(bytes, &mut index);
-                prev_color.b = read_8(bytes, &mut index);
-                prev_color.a = read_8(bytes, &mut index);
-            } else if (b1 & QOI_MASK_2) == QOI_OP_INDEX {
-                prev_color = seen_colors[b1 as usize];
-            } else if (b1 & QOI_MASK_2) == QOI_OP_DIFF {
-                prev_color.r += ((b1 >> 4) & 0x03) - 2;
-                prev_color.g += ((b1 >> 2) & 0x03) - 2;
-                prev_color.b += (b1 & 0x03) - 2;
-            } else if (b1 & QOI_MASK_2) == QOI_OP_LUMA {
-                let b2 = read_8(bytes, &mut index);
-                let vg = (b1 & 0x3f) - 32;
-                prev_color.r += vg - 8 + ((b2 >> 4) & 0x0f);
-                prev_color.g += vg;
-                prev_color.b += vg - 8 + (b2 & 0x0f);
-            } else if (b1 & QOI_MASK_2) == QOI_OP_RUN {
-                run = (b1 & 0x3f) as usize;
+            match QoiOp::from(b1) {
+                QoiOp::Rgb => {
+                    prev_color.r = read_8(bytes, &mut index);
+                    prev_color.g = read_8(bytes, &mut index);
+                    prev_color.b = read_8(bytes, &mut index);
+                }
+                QoiOp::Rgba => {
+                    prev_color.r = read_8(bytes, &mut index);
+                    prev_color.g = read_8(bytes, &mut index);
+                    prev_color.b = read_8(bytes, &mut index);
+                    prev_color.a = read_8(bytes, &mut index);
+                }
+                QoiOp::Index => {
+                    prev_color = seen_colors[b1 as usize];
+                }
+                QoiOp::Diff => {
+                    prev_color.r += ((b1 >> 4) & 0x03) - 2;
+                    prev_color.g += ((b1 >> 2) & 0x03) - 2;
+                    prev_color.b += (b1 & 0x03) - 2;
+                }
+                QoiOp::Luma => {
+                    let b2 = read_8(bytes, &mut index);
+                    let vg = (b1 & 0x3f) - 32;
+                    prev_color.r += vg - 8 + ((b2 >> 4) & 0x0f);
+                    prev_color.g += vg;
+                    prev_color.b += vg - 8 + (b2 & 0x0f);
+                }
+                QoiOp::Run => {
+                    run = (b1 & 0x3f) as usize;
+                }
+                QoiOp::Unknown => {}
             }
 
             seen_colors[prev_color.hash() % 64] = prev_color;
